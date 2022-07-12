f8c6ac130ea84a6e86cdb2ee5070f9c9b633cf18
mondalraj <mondal021002rajib@gmail.com>
2022-07-12 18:31:00 +0530

    fastapi setup initiated

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Blog(BaseModel):
+    id: int
+    title: str
+    description: str
+    user_id: int
 
 @app.get('/blog')
 def get_all_blogs():
@@ -9,3 +16,19 @@ def get_all_blogs():
 @app.get('/blog/{blog_id}')
 def get_blog_by_id(blog_id: int = Path(None, description="The Blog ID you want to view", ge=0)):
     return {"status": "success"}
+
+@app.post('/create-blog/{user_id}')
+def create_blog(blog: Blog):
+    return blog
+
+@app.put('/edit-blog/{user_id}/{blog_id}')
+def edit_blog(user_id: int, blog_id: int):
+    return user_id, blog_id
+
+@app.delete('/delete-blog/{user_id}/{blog_id}')
+def delete_blog(user_id: int, blog_id: int):
+    return {"status": "deleted"}
+
+@app.delete('/delete-all-blogs/{user_id}')
+def delete_blog(user_id: int):
+    return {"status": "deleted"}
